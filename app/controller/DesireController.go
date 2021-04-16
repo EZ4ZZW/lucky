640c6d1abb4799bb9c53ebb4139dcf380e6ccba0
EZ4ZZW <zhiwayzhang@outlook.com>
2021-04-17 01:25:17 +0800

    fix: add new variable

--- app/controller/DesireController.go
+++ app/controller/DesireController.go
@@ -10,13 +10,25 @@ import (
 )
 
 func AddDesire(c *gin.Context) {
-	desireJson := model.Desire{}
+	var desireJson model.Desire
 	desireValite := validate.DesireValidate
 	desireModel := model.Desire{}
 
 	if err := c.ShouldBindJSON(&desireJson); err != nil {
-		c.JSON(http.StatusOK, helper.ApiReturn(http.StatusOK, "绑定数据模型失败", err.Error()))
+		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeSuccess, "绑定数据模型失败", err.Error()))
 		return
 	}
 
+	desireMap := helper.Struct2Map(desireJson)
+
+	if res,err := desireValite.ValidateMap(desireMap, "add"); !res {
+		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeError, "数据校验失败", err.Error()))
+		return
+	}
+
+	res := desireModel.AddDesire(desireJson)
+
+	c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
+
 }
+
--- app/model/DesireModel.go
+++ app/model/DesireModel.go
@@ -7,14 +7,16 @@ import (
 )
 
 type Desire struct {
-	ID int `json:"id" gorm:"id"`
-	Desire string `json:"wish" gorm:"desire"`
-	WishmanName string `json:"wish_man_name" gorm:"wishman_qq"`
-	WishManWechat string `json:"wish_man_wechat" gorm:"wishman_wechat"`
-	WishManTel string `json:"wish_man_tel" gorm:"wishman_tel"`
-	State int `json:"state" gorm:"state"`
-	CreatAt time.Time `json:"creat_at" gorm:"creat_at"`
-	LightAt time.Time `json:"light_at" gorm:"light_at"`
+	ID            int       `json:"id" gorm:"id"`
+	Desire        string    `json:"wish" gorm:"desire"`
+	LightAt       time.Time `json:"light_at" gorm:"light_at"`
+	CreatAt       time.Time `json:"creat_at" gorm:"creat_at"`
+	WishmanName   string    `json:"wishman_name" gorm:"wishman_qq"`
+	WishmanQQ     string    `json:"wishman_qq" gorm:"wishman_qq"`
+	WishmanWechat string    `json:"wishman_wechat" gorm:"wishman_wechat"`
+	WishmanTel    string    `json:"wishman_tel" gorm:"wishman_tel"`
+	State         int       `json:"state" gorm:"state"`
+	Type          int       `json:"type" gorm:"type"`
 }
 
 func (Desire) TableName() string {
@@ -23,7 +25,7 @@ func (Desire) TableName() string {
 
 func (model *Desire) AddDesire(data Desire) helper.ReturnType {
 
-	err := db.Create(&data).Error
+	err := db.Model(&Desire{}).Omit("creat_at", "light_at").Create(&data).Error
 
 	if err != nil {
 		return helper.ReturnType{Status: common.CodeError, Msg: "添加愿望失败", Data: err.Error()}
