4190d1f97f482a991b1748eef8b7a236f1eb5eb0
EZ4ZZW <zhiwayzhang@outlook.com>
2021-04-17 03:12:25 +0800

    add: 增加实现愿望接口

--- app/model/DesireModel.go
+++ app/model/DesireModel.go
@@ -1,13 +1,14 @@
 package model
 
 import (
+	"github.com/gin-gonic/gin"
 	"lucky/app/common"
 	"lucky/app/helper"
 	"time"
 )
 
 type Desire struct {
-	ID            int       `json:"id" gorm:"id"`
+	ID            int       `json:"wish_id" gorm:"id"`
 	Desire        string    `json:"wish" gorm:"desire"`
 	LightAt       time.Time `json:"light_at" gorm:"light_at"`
 	CreatAt       time.Time `json:"creat_at" gorm:"creat_at"`
@@ -32,4 +33,29 @@ func (model *Desire) AddDesire(data Desire) helper.ReturnType {
 	}
 
 	return helper.ReturnType{Status: common.CodeSuccess, Msg: "添加愿望成功", Data: data}
+}
+
+func (model *Desire) AchieveDesire(data Desire) helper.ReturnType {
+
+	var desire Desire
+
+	err := db.Model(&Desire{}).Where("id = ?", data.ID).Find(&desire).Error
+
+	if err != nil {
+		return helper.ReturnType{Status: common.CodeError, Msg: "实现愿望失败", Data: err.Error()}
+	}
+
+	if desire.State == 0 {
+		desire.State = 1
+		desire.LightAt = time.Now()
+		err := db.Model(&Desire{}).Update(&desire).Error
+		if err != nil {
+			return helper.ReturnType{Status: common.CodeError, Msg: "实现愿望失败,数据库错误", Data: err.Error()}
+		}
+		return helper.ReturnType{Status: common.CodeSuccess, Msg: "实现愿望成功", Data: gin.H{
+			"wish_state": true,
+		}}
+	}
+
+	return helper.ReturnType{Status: common.CodeSuccess, Msg: "实现愿望失败，该愿望已经被别人抢先实现了", Data: 1}
 }
