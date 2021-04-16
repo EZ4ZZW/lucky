bcb19e35954cce9ff4c7b837d9c62d2979114e4c
EZ4ZZW <zhiwayzhang@outlook.com>
2021-04-17 03:13:18 +0800

    add: 添加从session获取id

--- app/helper/formatter.go
+++ app/helper/formatter.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"reflect"
 	"strings"
@@ -10,6 +11,15 @@ type ReturnType struct {
 	Status int
 	Msg    string
 	Data   interface{}
+}
+
+// 从session获取用户ID
+func GetUserIdFromSession(c *gin.Context) int {
+	session := sessions.Default(c)
+	if id := session.Get("user_id"); id != nil {
+		return id.(int)
+	}
+	return 0
 }
 
 // 结构体转换为map
